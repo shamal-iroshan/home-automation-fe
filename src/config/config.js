2c26ca4bef1c56cf470c888b2733870e77c70a55
shamal-iroshan <shamal@fcodelabs.com>
2021-12-18 22:51:24 +0530

    added base url

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://127.0.0.1:5000/"
+// export const BASE_URL = "http://127.0.0.1:5000/"
+export const BASE_URL = "http://finalproject-env-1.eba-5pprkiwz.ap-southeast-1.elasticbeanstalk.com/"
